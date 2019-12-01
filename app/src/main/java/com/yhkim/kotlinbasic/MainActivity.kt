ff49c40a6f14fa0e784f7e917b70756243e62a1c
kvoyant <kvoyant@gmail.com>
2019-12-01 11:24:49 +0900

    로그검색 활용 - Logcat

--- app/src/main/java/com/yhkim/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/yhkim/kotlinbasic/MainActivity.kt
@@ -2,6 +2,7 @@ package com.yhkim.kotlinbasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,11 +20,16 @@ class MainActivity : AppCompatActivity() {
 //            Ctrl + k 커밋
             var inputString = inputEdt.text.toString() // get => 내용물 받아오기.
             resultTxt.text = inputString // set => 내용물 설정하기.
+
+//            확인 버튼이 눌렸는지 로그
+            Log.d("확인버튼 클릭", inputString)
         }
 
         okBtn.setOnLongClickListener {
 
             resultTxt.text = "초기상태"
+
+            Log.d("확인버튼롱클릭", "텍스트뷰 초기화")
 
             return@setOnLongClickListener true
         }
